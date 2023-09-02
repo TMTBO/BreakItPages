fe50daa8f0ddce350545d32ded5b43638a39cd2b
TobyoTenma <tmtbo@hotmail.com>
2023-09-02 18:50:56 +0800

    added: add ad.html

--- docs/_static/js/ad.js
+++ docs/_static/js/ad.js
@@ -1,3 +1,3 @@
-$(document).ready(function() {
+document$.ready(function() {
   $("nav.navbar.navbar-expand-lg.navbar-dark.bg-primary.mb-3").after($("<div></div>").load("/docs/_static/html/ad.html")); 
 });
